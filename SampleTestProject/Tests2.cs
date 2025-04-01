0e982caf6afe72dfe85d34318e6d2a987272f44e
Sanjeel Nath <sanjeel.nath@vulcan.co>
2025-04-01 15:26:42 +1300

    add another test

---
+++ SampleTestProject/Tests2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTestProject
+{
+    class Tests2
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void PassingTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
