4bfa66aeb3c090e93d19b0e712b688e82029f435
Sanjeel Nath <sanjeel.nath@vulcan.co>
2025-04-07 13:55:20 +1200

    more tests!

--- ConsoleApp/Calc.cs
+++ ConsoleApp/Calc.cs
@@ -6,4 +6,9 @@ public class Calc
     {
         return a + b;
     }
+
+    public int subtract(int a, int b)
+    {
+        return a - b;
+    }
 }
--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -1,4 +1,5 @@
 using BankAccountNS;
+using ConsoleApp;
 
 namespace TestProject2
 {
@@ -18,6 +19,22 @@ namespace TestProject2
 
             Assert.That(account.Balance, Is.EqualTo(beginningBalance));
             Assert.That(account.CustomerName, Is.EqualTo("Sanjeel"));
+        }
+
+        [Test]
+        public void AdditionTest()
+        {
+            var calc = new Calc();
+            var result = calc.Add(5, 5);
+            Assert.That(result, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void MultiplicationTest()
+        {
+            var calc = new Calc();
+            var result = calc.multiply(4, 6);
+            Assert.That(result, Is.EqualTo(24));
         }
     }
 }
