b089deb855983c906ece1e7cb948d77d07dc82c8
Sanjeel Nath <sanjeel.nath@vulcan.co>
2025-05-08 17:50:52 +1200

    1 line change

--- ConsoleApp/BankAccount.cs
+++ ConsoleApp/BankAccount.cs
@@ -17,7 +17,7 @@ public class BankAccount
 
     public static void Main()
     {
-        BankAccount ba = new BankAccount("Sanjeel", 11.99);
+        var ba = new BankAccount("Sanjeel", 11.99);
         Console.WriteLine("Current balance is ${0} for {1}", ba.Balance, ba.CustomerName);
     }
 }
